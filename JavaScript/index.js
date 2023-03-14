b549982056cb596100a192462bc5133d29ebe21b
GadReda121 <khtabjab@gmail.com>
2023-03-15 00:02:43 +0200

    project 18

---
+++ JavaScript/index.js
@@ -0,0 +1,142 @@
+$(document).ready(function () {
+    $(".home i").click(function (e) { 
+        e.preventDefault();
+        window.scroll({
+            top: 950
+        })
+    });
+    // Sabeeh
+    
+    var childbtn1 = document.querySelector('.sabeeh .colONE .child1'),
+    count1 = 0;
+    childbtn1.onclick = function() {
+    count1 += 1;
+    var spaN1 = document.querySelector(".sabeeh .colONE .child1 h6 span");
+    spaN1.innerHTML = count1;
+    if(count1 == 33){
+        count1 = 0;
+    }
+    }
+
+    var childbtn2 = document.querySelector('.sabeeh .colONE .child2'),
+    count2 = 0;
+    childbtn2.onclick = function() {
+    count2 += 1;
+    var spaN2 = document.querySelector(".sabeeh .colONE .child2 h6 span");
+    spaN2.innerHTML = count2;
+    if(count2 == 33){
+        count2 = 0;
+    }
+    }
+
+    var childbtn3 = document.querySelector('.sabeeh .colONE .child3'),
+    count3 = 0;
+    childbtn3.onclick = function() {
+        count3 += 1;
+    var spaN3 = document.querySelector(".sabeeh .colONE .child3 h6 span");
+    spaN3.innerHTML = count3;
+    if(count3 == 33){
+        count3 = 0;
+    }
+    }
+
+    var childbtn4 = document.querySelector('.sabeeh .colTwo .child1'),
+    count4 = 0;
+    childbtn4.onclick = function() {
+    count4 += 1;
+    var spaN4 = document.querySelector(".sabeeh .colTwo .child1 h6 span");
+    spaN4.innerHTML = count4;
+    if(count4 == 33){
+        count4 = 0;
+    }
+    }
+
+    var childbtn5 = document.querySelector('.sabeeh .colTwo .child2'),
+    count5 = 0;
+    childbtn5.onclick = function() {
+    count5 += 1;
+    var spaN5 = document.querySelector(".sabeeh .colTwo .child2 h6 span");
+    spaN5.innerHTML = count5;
+    if(count5 == 33){
+        count5 = 0;
+    }
+    }
+
+    var childbtn6 = document.querySelector('.sabeeh .colTwo .child3'),
+    count6 = 0;
+    childbtn6.onclick = function() {
+    count6 += 1;
+    var spaN6 = document.querySelector(".sabeeh .colTwo .child3 h6 span");
+    spaN6.innerHTML = count6;
+    if(count6 == 33){
+        count6 = 0;
+    }
+    }
+
+    var childbtn7 = document.querySelector('.sabeeh .colThree .child1'),
+    count7 = 0;
+    childbtn7.onclick = function() {
+    count7 += 1;
+    var spaN7 = document.querySelector(".sabeeh .colThree .child1 h6 span");
+    spaN7.innerHTML = count7;
+    if(count7 == 33){
+        count7 = 0;
+    }
+    }
+
+    var childbtn8 = document.querySelector('.sabeeh .colThree .child2'),
+    count8 = 0;
+    childbtn8.onclick = function() {
+    count8 += 1;
+    var spaN8 = document.querySelector(".sabeeh .colThree .child2 h6 span");
+    spaN8.innerHTML = count8;
+    if(count8 == 33){
+        count8 = 0;
+    }
+    }
+
+    var childbtn9 = document.querySelector('.sabeeh .colThree .child3'),
+    count9 = 0;
+    childbtn9.onclick = function() {
+    count9 += 1;
+    var spaN9 = document.querySelector(".sabeeh .colThree .child3 h6 span");
+    spaN9.innerHTML = count9;
+    if(count9 == 33){
+        count9 = 0;
+    }
+    }
+
+    var childbtn10 = document.querySelector('.sabeeh .colFour .child1'),
+    count10 = 0;
+    childbtn10.onclick = function() {
+    count10 += 1;
+    var spaN10 = document.querySelector(".sabeeh .colFour .child1 h6 span");
+    spaN10.innerHTML = count10;
+    if(count10 == 33){
+        count10 = 0;
+    }
+    }
+
+    var childbtn11 = document.querySelector('.sabeeh .colFour .child2'),
+    count11 = 0;
+    childbtn11.onclick = function() {
+    count11 += 1;
+    var spaN11 = document.querySelector(".sabeeh .colFour .child2 h6 span");
+    spaN11.innerHTML = count11;
+    if(count11 == 33){
+        count11 = 0;
+    }
+    }
+
+    var childbtn12 = document.querySelector('.sabeeh .colFour .child3'),
+    count12 = 0;
+    childbtn12.onclick = function() {
+    count12 += 1;
+    var spaN12 = document.querySelector(".sabeeh .colFour .child3 h6 span");
+    spaN12.innerHTML = count12;
+    if(count12 == 33){
+        count12 = 0;
+    }
+    }
+
+});
